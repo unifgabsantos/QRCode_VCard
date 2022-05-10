1b4e04ae41d4309758cb2e91a06728e67964578d
Gabriel Lopes <57074487+unifgabsantos@users.noreply.github.com>
2022-05-10 08:37:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from VCard import Bot
-Bot(dados={
-    "Name":"Gabriel Lopes Santos",
-    "FirstName":"Gabriel",
-    "LastName":"Santos",
-    "Company":"Example",
-    "Title":"Example",
-    "Address":"Rua Example, 123",
-    "Celular":"123",
-    "Email":"gabriel@example.com",
-    "url":"https://google.com"
-}).create()
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+wb = load_workbook('Dados.xlsx') 
+ws = wb['Sheet1']
+for linha in range(2,165):
+    firstName = ws["A"+str(linha)]._value
+    lastName = ws["B"+str(linha)]._value
+    fullName = f"{firstName} {lastName}"
+    title = ws["G"+str(linha)]._value
+    cel = f'+55{ws["H"+str(linha)]._value}'
+    email = ws["I"+str(linha)]._value
+    address = f'{ws["K"+str(linha)]._value}, {(ws["L"+str(linha)]._value).replace("ã","a")}, {ws["M"+str(linha)]._value}- {ws["J"+str(linha)]._value}'
+    Bot(dados={
+        "Name":fullName,
+        "FirstName":firstName,
+        "LastName":lastName,
+        "Title":title,
+        "Address":address,
+        "Celular":cel,
+        "Email":email,
+    }).create()
